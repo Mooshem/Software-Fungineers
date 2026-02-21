7b50c69727a1cf7930302386146cacdad2285f9b
Donovan Thach <thachd@oregonstate.edu>
2026-02-21 02:52:24 -0800

    Added UI addtions to var_block.gd script.

--- coding-cubed/Blocks/var_block.gd
+++ coding-cubed/Blocks/var_block.gd
@@ -5,6 +5,9 @@ Documentation : https://docs.godotengine.org/en/4.4/tutorials/scripting/gdscript
 
 extends StaticBody3D
 
+# Environment variables to be initialized.
+@onready var ui_display = $UIDisplay
+var ui_visible: bool = false
 var raycast: RayCast3D
 
 func _ready() -> void:
@@ -21,11 +24,35 @@ func _unhandled_input(event: InputEvent) -> void:
 	pass
 	
 func handle_user_interaction(event: InputEvent) -> void:
-	var target_block = raycast.get_collider()
-	if !raycast:
+	"""Handles user inputs directed towards interacting with a block."""
+	# Checks if during an interaction event if the player's raycast is colliding with a block.
+	if !raycast or !raycast.is_colliding():
 		return
-	else:
-		if Input.is_action_just_pressed("interact") and target_block.scene_file_path == "Block/var_block.tscn":
-			print("Working")
-			return
-	pass
+		
+	# Gets the current block that the player is looking at.
+	var target_block = raycast.get_collider()
+	if target_block != self:
+		return
+		
+	# Handle interaction.
+	if Input.is_action_just_pressed("interact"):
+		toggle_ui()
+
+func toggle_ui() -> void:
+	if !ui_display:
+		return
+		
+	ui_visible = !ui_visible
+	ui_display.visible = ui_visible
+	
+	if ui_visible:
+		# Get the direction of the vector from raycast.
+		var hit_normal = raycast.get_collision_normal()
+		
+		# Position 1 unit away from block in normal direction.
+		ui_display.position = hit_normal
+		
+		# Make the UI face the player (opposite of raycast vector direction).
+		var raycast_direction = -raycast.global_transform.basis.z
+		ui_display.look_at(ui_display.global_position + raycast_direction)
+		
