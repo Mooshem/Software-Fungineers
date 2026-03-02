a28383c2ff98f622340818dadd7b07f72d374cf9
Donovan Thach <thachd@oregonstate.edu>
2026-03-01 17:07:11 -0800

    Restructuring UI on canvaslayer and new scripts.

--- coding-cubed/Blocks/var_block.gd
+++ coding-cubed/Blocks/var_block.gd
@@ -5,67 +5,35 @@ Documentation : https://docs.godotengine.org/en/4.4/tutorials/scripting/gdscript
 
 extends StaticBody3D
 
-# Environment variables to be initialized.
-@onready var ui_display = $UIDisplay
-@onready var ui_backDisplay = $UIBackDisplay
-var ui_visible: bool = false
+# Global variables to be set.
 var raycast: RayCast3D
-var var_name
+var var_name: String = ""
+var var_val: String = ""
+var player = null
 
 func _ready() -> void:
-	var player = get_tree().get_first_node_in_group("player")
+	# Get the player node from Player scene.
+	player = get_tree().get_first_node_in_group("player")
 	if player:
+		# Get the raycast from the player object.
 		raycast = player.get_node("Head/Camera3D/RayCast3D")
 		
-	# Set UI to invisible by default.
-	if ui_display:
-		ui_display.visible = false
-	if ui_backDisplay:
-		ui_backDisplay.visible = false
-
-
-func _process(delta: float) -> void:
-	pass
-
 func _unhandled_input(event: InputEvent) -> void:
-	handle_user_interaction(event)
-	pass
-	
-func handle_user_interaction(event: InputEvent) -> void:
-	"""Handles user inputs directed towards interacting with a block."""
-	# Checks if during an interaction event if the player's raycast is colliding with a block.
+	# If raycast doesn't exist, return.
 	if !raycast or !raycast.is_colliding():
 		return
-		
-	# Gets the current block that the player is looking at.
-	var target_block = raycast.get_collider()
-	if target_block != self:
+	# If colliding object isn't var_block on raycast, then return.
+	if raycast.get_collider() != self:
 		return
-		
-	# Handle interaction.
+	# If player interacts with block, open player menu.
 	if Input.is_action_just_pressed("interact"):
-		toggle_ui()
-
-func toggle_ui() -> void:
-	if !ui_display:
+		_open_player_menu()
+		
+func _open_player_menu() -> void:
+	# Don't open if player object doesn't exist.
+	if !player:
 		return
 		
-	ui_visible = !ui_visible
-	ui_display.visible = ui_visible
-	ui_backDisplay.visible = ui_visible
-	
-	if ui_visible:
-		# Get the forward direction of the raycast.
-		var raycast_direction = raycast.global_transform.basis.z
-		
-		# Get block center to open UI from.
-		var block_center = global_position + Vector3(0.5, 0.5, 0.5)
-		
-		# Position the UI 1.25 units in the opposite direction to raycast point from block center.
-		ui_display.global_position = block_center + (raycast_direction * 1.25)
-		ui_backDisplay.global_position = block_center + (raycast_direction * 1.25)
-		
-		# Make UI face the player.
-		ui_display.look_at(ui_display.global_position - raycast_direction)
-		ui_backDisplay.look_at(ui_backDisplay.global_position + raycast_direction)
-		
+	# Get the menu of the variable block open.
+	var menu = player.get_node("CanvasLayer/VariableBlockMenu")
+	menu.open_for_block(self)
