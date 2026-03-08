a57ead42f641f14ad70a01f81db357d7e5c63509
Daniel Martin <danielamartin314@gmail.com>
2026-03-07 19:21:37 -0800

    Created end block that takes user back to main menu when power is recieved. Condition must be set MANUALLY per level

--- coding-cubed/Levels/level_select.gd
+++ coding-cubed/Levels/level_select.gd
@@ -2,6 +2,8 @@ extends Control
 
 const MAIN_MENU_SCENE := "res://Levels/main_menu.tscn"
 const SANDBOX_SCENE := "res://Levels/sandbox.tscn"
+const LEVEL2_SCENE := "res://Levels/Level2.tscn"
+const LEVEL5_SCENE := "res://Levels/Level5.tscn"
 const FADE_DURATION := 0.25
 
 @onready var notice_label: Label = $Center/Panel/VBox/Notice
@@ -17,7 +19,7 @@ func _on_level_one_button_pressed() -> void:
 	_transition_to_scene(SANDBOX_SCENE)
 
 func _on_level_two_button_pressed() -> void:
-	notice_label.text = "Level 2 is a placeholder for now."
+	_transition_to_scene(LEVEL2_SCENE)
 
 func _on_back_button_pressed() -> void:
 	_transition_to_scene(MAIN_MENU_SCENE)
